011ed66cb5e71e25dd6c99dc925ee26130a7c9cb
John Christian Vicente <johnc.vicente1@gmail.com>
2024-11-25 09:19:13 +0800

    refactor: use AppRight for main application

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from app.app import AppTop  # Change this to AppLeft, AppRight, or AppTop for navigation
+from app.app import (
+    AppRight,
+)  # Change this to AppLeft, AppRight, or AppTop for navigation
 from app.db import init_db
 
 if __name__ == "__main__":
     init_db()
-    app = AppTop()  # Change this to AppLeft, AppRight, or AppTop for navigation
+    app = AppRight()  # Change this to AppLeft, AppRight, or AppTop for navigation
     app.mainloop()
